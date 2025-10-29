59750b9910149990d96a7ef7a65977ddfd6b20fa
tech-sgt-web <tech_dmarketing@sgtuniversity.org>
2025-10-29 15:33:34 +0530

    Trigger auto deploy

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -330,7 +330,7 @@ const LoginPage = () => {
               textAlign="center"
               sx={{ fontSize: { xs: '0.95rem', sm: '1.1rem' } }}
             >
-              Enter your credentials to access your dashboards
+              Enter your credentials to access your dashboard
             </Typography>
             
             {error && (
